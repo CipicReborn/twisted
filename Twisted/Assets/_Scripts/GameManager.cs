5c966c92f0de24825cd4a5ee2512c81442f088bb
Cyprien Larrouy <cyprien.larrouy@gmail.com>
2017-11-01 19:33:36 +0100

    Added UI Screen Transitions

---
+++ Twisted/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartGame () {
+        ScreenManager.Instance.OpenScreen(ScreenManager.Instance.GameScreen);
+    }
+
+    public void GameOver () {
+        ScreenManager.Instance.OpenScreen(ScreenManager.Instance.GameOverScreen);
+    }
+
+    public void GoToMainMenu () {
+        ScreenManager.Instance.OpenScreen(ScreenManager.Instance.TitleCard);
+    }
+}
---
+++ Twisted/Assets/_Scripts/ScreenManager.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ScreenManager : MonoBehaviour {
+    
+    #region STATIC
+    public static ScreenManager Instance {
+        get {
+            return instance;
+        }
+    }
+
+    private static ScreenManager instance;
+    #endregion
+
+    #region PUBLIC
+    public Animator StartScreen;
+    public Animator TitleCard;
+    public Animator GameScreen;
+    public Animator GameOverScreen;
+
+    public void OpenScreen (Animator screenToOpen) {
+        if (m_currentlyOpenScreen == screenToOpen)
+            return;
+
+        m_screenToOpen = screenToOpen;
+        TransitionToNewScreen();
+    }
+
+    #endregion
+
+    #region PRIVATE
+
+    private const string OPEN_TRANSITION_NAME = "isOpen";
+    private const string OPEN_STATE_NAME = "Open";
+    private const string CLOSED_STATE_NAME = "Closed";
+
+    private Animator m_currentlyOpenScreen;
+    private Animator m_screenToOpen;
+    private int m_isOpenParameter;
+
+
+    void OnEnable () {
+        instance = this;
+        m_isOpenParameter = Animator.StringToHash(OPEN_TRANSITION_NAME);
+        if (StartScreen == null)
+            return;
+        OpenScreen(StartScreen);
+    }
+
+
+    void TransitionToNewScreen () {
+        if (m_currentlyOpenScreen != null) {
+            AnimateScreenClosingAndTransition();
+        }
+        else {
+            AnimateScreenOpening();
+        }
+    }
+
+    void AnimateScreenClosingAndTransition () {
+        m_currentlyOpenScreen.SetBool(m_isOpenParameter, false);
+        StartCoroutine(WaitForScreenState(m_currentlyOpenScreen, CLOSED_STATE_NAME, Transition));
+    }
+
+    void Transition () {
+        AnimateScreenOpening();
+        FinaliseScreenClosing();
+    }
+    void FinaliseScreenClosing () {
+        m_currentlyOpenScreen.gameObject.SetActive(false);
+        m_currentlyOpenScreen = null;
+    }
+
+    void AnimateScreenOpening () {
+        m_screenToOpen.gameObject.SetActive(true);
+        m_screenToOpen.transform.SetAsLastSibling();
+        m_screenToOpen.SetBool(m_isOpenParameter, true);
+        StartCoroutine(WaitForScreenState(m_screenToOpen, OPEN_STATE_NAME, RecordScreenAsOpened));
+    }
+
+    void RecordScreenAsOpened () {
+        m_currentlyOpenScreen = m_screenToOpen;
+        m_screenToOpen = null;
+    }
+
+    IEnumerator WaitForScreenState (Animator screenAnimator, string targetStateName, System.Action callback) {
+        bool stateReached = false;
+        while (!stateReached) {
+            if (!screenAnimator.IsInTransition(0)) {
+                stateReached = screenAnimator.GetCurrentAnimatorStateInfo(0).IsName(targetStateName);
+            }
+            yield return new WaitForEndOfFrame();
+        }
+        callback();
+    }
+
+
+    #endregion
+}
